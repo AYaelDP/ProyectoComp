32fe4cd27f9b1331cd58212603bd07e0299675fe
AYaelDP <128731060+AYaelDP@users.noreply.github.com>
2023-11-25 22:13:43 -0600

    Add files via upload

---
+++ ASDR_SQL/ASDR_SQL/Principal.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Principal {
+
+    static boolean existenErrores = false;
+
+    public static void main(String[] args) throws IOException {
+        ejecutarPrompt();
+    }
+
+    private static void ejecutarPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print(">>> ");
+            String linea = reader.readLine();
+            if(linea == null) break; // Presionar Ctrl + D
+            ejecutar(linea);
+            existenErrores = false;
+        }
+    }
+
+    private static void ejecutar(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        ASDR parser = new ASDR(tokens);
+        boolean resultado = parser.parse();
+
+        /*for(Token token : tokens){
+            System.out.println(token);
+        }*/
+
+        if (resultado) {
+            System.out.println("Sentencia correcta.");
+        }else{
+            System.out.println("Sentencia no aceptada.");
+        }
+        
+    }
+
+    /*
+    El método error se puede usar desde las distintas clases
+    para reportar los errores:
+    Interprete.error(....);
+     */
+    static void error(int linea, String mensaje){
+        reportar(linea, "", mensaje);
+    }
+
+    private static void reportar(int linea, String donde, String mensaje){
+        System.err.println(
+                "[linea " + linea + "] Error " + donde + ": " + mensaje
+        );
+        existenErrores = true;
+    }
+
+}
